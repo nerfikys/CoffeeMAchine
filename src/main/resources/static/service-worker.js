c4c9cd198fcc5c10e66657bcca05f72ba8090258
komerbek <komerbek.lukmanov@gmail.com>
2021-04-29 14:00:06 +0300

    heroku-pwa

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -1,1 +1,31 @@
 console.log('Hello from service-worker.js');
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+
+
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+
+workbox.routing.registerRoute(
+  new RegExp('\.png$'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'images-cache',
+  })
+);
