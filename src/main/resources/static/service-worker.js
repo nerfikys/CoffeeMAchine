28848fa7c4def77dc46df3305097d904de651771
komerbek <komerbek.lukmanov@gmail.com>
2021-05-04 18:09:19 +0300

    v. 1.1.1 - исправление ошибок

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -2,28 +2,28 @@ console.log('Hello from service-worker.js');
 
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-   'http://localhost:8088/',
-  '/main',
-  '/login',
-  '/registration',
-  '/user'
+    'https://health-pwa.herokuapp.com/',
+    '/main',
+    '/login',
+    '/registration',
+    '/user'
 ];
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
-  // Respond with a cached resource, or else fetch from network.
-  event.respondWith((async () => {
-    const response = await caches.match(event.request);
-    return response || fetch(event.request);
-  })());
+    // Respond with a cached resource, or else fetch from network.
+    event.respondWith((async() => {
+        const response = await caches.match(event.request);
+        return response || fetch(event.request);
+    })());
 });
